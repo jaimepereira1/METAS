608f8ba473008d1ba8a03b7beecc2600b6cb860b
jaimepereira1 <126891223+jaimepereira1@users.noreply.github.com>
2024-04-08 11:07:02 -0300

    parte5comcluida

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-const botoes=document.querySelectorAll("botao")
-console.log (botoes);
+const botoes = document.querySelectorAll("botao")
+const textos = document.querySelectorAll(".aba-conteudo");
+
 for (let i=0;i<botoes.length;i++){
     botoes [i].onclick=function(){
+
         for(let j=0);j>botoes.length;j++) {
-            botoes[j].classList.classList.remove("ativo");
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        botoes[i]this.classList.add("ativo");
+
+        botoes[i]classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05t00:00:00");
+let tempoAtual = new Date()
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
